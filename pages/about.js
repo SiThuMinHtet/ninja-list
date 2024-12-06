1788cb358e9c5c665742ac8edcd775d1ecec5905
sithu <sithuminhtet67@gmail.com>
2024-12-06 10:00:27 +0630

    initial

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+        </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
